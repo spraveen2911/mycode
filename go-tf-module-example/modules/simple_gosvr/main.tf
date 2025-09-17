1e02d6599e79d0a53b09afba6e366c84ed96f5f6
spraveen2911 <praveen.pravee2911@gmail.com>
2025-09-17 18:57:16 +0000

    go microservice and local modules

---
+++ go-tf-module-example/modules/simple_gosvr/main.tf
@@ -0,0 +1,18 @@
+/* main.tf */
+
+resource "docker_image" "gosvr" {
+  # the name of our go image
+  name         = var.image_name  // define this
+  keep_locally = true            // keep image after "destroy"
+}
+
+resource "docker_container" "gosvr" {
+  image = docker_image.gosvr.image_id
+  network_mode = "bridge"
+  name = var.container_name      // define this
+  ports {
+    internal = 9876
+    external = 2224
+  }
+}
+
